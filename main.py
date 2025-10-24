76210a9e0721340cb05a18dd191ff1e625bb7438
Fedo <thf111@gmail.com>
2025-10-24 22:37:17 +0200

    upravy DB

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ cur = conn.cursor()
 
 
 conn.execute("DROP TABLE IF EXISTS users;")
-conn.execute("CREATE TABLE IF NOT EXISTS users (name TEXT);")
-conn.execute("INSERT INTO users VALUES ('Alice');")
-conn.execute("INSERT INTO users VALUES ('Bob');")
-conn.execute("INSERT INTO users VALUES ('Charlie');")
+conn.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, email TEXT, age INTEGER);")
+conn.execute("INSERT INTO users VALUES ('Alice', 'alice@example.com', 30);")
+conn.execute("INSERT INTO users VALUES ('Bob', 'bob@example.com', 25);")
+conn.execute("INSERT INTO users VALUES ('Charlie', 'charlie@example.com', 35);")
 
 conn.commit()
 
-result = conn.execute("select * from users").fetchall()
-conn.close()
-
-print(result)
+print(conn.execute("SELECT * FROM users ORDER BY age").fetchall())
